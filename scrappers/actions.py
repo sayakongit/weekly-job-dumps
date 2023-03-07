9fb3d2feb5e4d931f89c6d076555b1abb690a64f
Sayak Saha <83216382+sayakongit@users.noreply.github.com>
2023-03-07 21:33:50 +0530

    added naukri and moster scrapper

--- scrappers/actions.py
+++ scrappers/actions.py
@@ -1,7 +1,101 @@
-from utils.actions import read_sheet_data
+from urllib.parse import urlencode
+from .constants import NAUKRI_JOB_DETAILS_HEADERS, NAUKRI_ALL_JOBS_HEADERS, MONSTER_API_HEADERS, MONSTER_PAYLOAD, MONSTER_JOB_SEARCH_ENDPOINT
+import requests
+import re
 
-def test():
-    data, sheet = read_sheet_data('Jobs', 'Test')
-    print(data)
+def cleanhtml(raw_html):
+    CLEANR = re.compile('<.*?>')
+    cleantext = re.sub(CLEANR, '', raw_html)
+    return cleantext
+
+def naukri_search_by_keyword(keyword, location, sort_by):
+    url = "https://www.naukri.com/jobapi/v3/search?"
+    payload = {
+        "urlType": "search_by_keyword",
+        "searchType": "adv",
+        "keyword": keyword,
+        "location": location,
+        "sort": sort_by,
+        "noOfResults": 100
+    }
     
-test()
+    response = requests.get(url, params=urlencode(payload),  headers=NAUKRI_ALL_JOBS_HEADERS)
+    search_results = response.json()
+    status = response.status_code
+    all_jobs = []
+    if status == 200 and 'jobDetails' in search_results:
+        for job in search_results['jobDetails']:
+            data = {}
+            data['title'] = job['title']
+            data['company_name'] = job['companyName']
+            data['skills'] = job['tagsAndSkills'].split(',')
+            
+            for placeholder in job['placeholders']:
+                if placeholder['type'] == 'experience':
+                    data['experience'] = placeholder['label']
+                if placeholder['type'] == 'salary':
+                    data['salary'] = placeholder['label']
+                if placeholder['type'] == 'location':
+                    data['location'] = placeholder['label']
+                    
+            data['jd_url'] = 'www.naukri.com' + job['jdURL']
+            data['job_highlights'] = cleanhtml(job['jobDescription'])
+            data['time_created'] = job['footerPlaceholderLabel']
+            try:
+                data['review_count'] = job['ambitionBoxData']['ReviewsCount']
+                data['ratings'] = job['ambitionBoxData']['AggregateRating']
+            except:
+                data['review_count'] = ''
+                data['ratings'] = ''
+            all_jobs.append(data)
+
+        return all_jobs
+    return {'error': 'No data found'}
+    
+    
+def foundit_search_by_keyword(keyword, location, sort_by):
+    url = MONSTER_JOB_SEARCH_ENDPOINT
+    MONSTER_PAYLOAD['query'] = keyword
+    
+    print('SORT', sort_by)
+    if location != '':
+        MONSTER_PAYLOAD['locations'] = location
+    if sort_by != '':
+        MONSTER_PAYLOAD['sort'] = "2"
+    
+    try:
+        search_results = requests.get(url, params=urlencode(MONSTER_PAYLOAD),  headers=MONSTER_API_HEADERS).json()
+    except Exception as e:
+        print(e)
+        return {'error': 'Could not fetch data'}
+    
+    jobs = []
+    try:
+        if search_results['jobSearchStatus'] == 200 and len(search_results['jobSearchResponse']['data']) > 0:
+
+            for job in search_results['jobSearchResponse']['data']:
+                if len(job) > 3:
+                    data = {}
+                    data['title'] = job['title']
+                    data['company_name'] = job['companyName']
+                    try:
+                        data['skills'] = job['skills'].split(',')
+                    except:
+                        data['skills'] = []
+                    
+                    data['experience'] = job['exp'] if job['exp'] != '' else 'Not disclosed'
+                    data['salary'] = job['salary'] if job['salary'] != '' else 'Not disclosed'
+                    data['location'] = job['locations']
+                            
+                    data['time_created'] = job['postedBy']
+
+                    data['review_count'] = ''
+                    data['ratings'] = ''
+                    
+                    jobs.append(data)
+        else:
+            return {'error': 'No data found'}
+    except Exception as e:
+        print(e)
+        return {'error': 'Error'}
+    return jobs
---
+++ scrappers/constants.py
@@ -0,0 +1,35 @@
+NAUKRI_JOB_DETAILS_HEADERS = {
+        "appid": "121",
+        "systemid": "Naukri",
+        "referer": "https://www.naukri.com",
+        "accept": "application/json",
+        "accept-encoding": "gzip, deflate, br",
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36",
+        "cookie": "test=naukri.com; _did=94f95f55cc; _odur=3e8bfcd1f8; MYNAUKRI[UNID]=d73628c98bf9463899ca5647ef320277; PS=1a9f41e03c248e07699fd1bb97a84ec8a8e747a11f7c21a714980a7b8356e4db1edd7a843282321a; _ff_ds=0319121001675515532-C4A1442688C4-110C2AD7CDC5; PS=1a9f41e03c248e07699fd1bb97a84ec8a8e747a11f7c21a714980a7b8356e4db1edd7a843282321a; _t_ds=246197447; ACTIVE=1676870709; _t_s=direct; tStp=1676876411909; bm_sz=54869ACB3866D6003F636D017C1D1785~YAAQM0w5F+8cL1qGAQAA1RRUbxLocjkiGZt2oK6wQTyib+9ir83fiEVHJhmDGOwbU7aRojsOca4fqMyCtEyfMKhA5lgtqipwCgfkTjDcIDN5WzppKVDHGqYmV4ewLYSMtI3pMYc5t2E0mNnaIDhOnvIXVcH8sJfBzhpEioRW4dfN5+C8U3WobXAHHO4hZuFSmeDlt7Rn3RkZh0eFV/R7rbWzOECHiR3Kxf9kBDypvebD7Tp6khX4p4PBBLqAb/VhiQIzbAGthn+fE3AzQANaTYXUy8j0qOlENlHGT5e3d3UNMGM=~4407875~3421506; PHPSESSID=5495dfd2bec25c6bc991cf02b0b2683f; bm_mi=0AC1B17495B74E5F326A34295F26D772~YAAQM0w5F/QcL1qGAQAAVRVUbxKecvLmiDvJA41haIRZoQ6i4aDTW9ARLf3oGZiJhlp3wZmD1c/3wNqneYo+ffZtd+LfXZv9VVUt6Kk9So4D5MtsWwOcp4hzkrQhE8AcfwzfjqWzBP8DAs91txi93tSDsrUFLT3WrpyB5VsDFbtwRy72jgq+IFo9cp+hTA0r9QV4h+H03SKNFQj0MpKhtZomUJtI5T1ch3iHJmYAM491QnfZWmoGu4PWpaD1h2WFR8K6U2+T3LyJAQvfblIEK2RR/b76Qg+usAe4VZL/3WQjCEmy8+pu4VMe3Kprc4EqRhslQO9Zq6wknziPs9k=~1; _t_us=63F38A44; ak_bmsc=811F26B3BE53E719465BBC44B0EC0C1B~000000000000000000000000000000~YAAQM0w5F1UdL1qGAQAAjhtUbxLBK98g1nTS0f2XbupMIN7sFYdWRQaukjHxaH9YX2AWDr0UXV8kTteH+T/38fw30zmDNeB0qTZ3flr1Oxp+E7cycI299sZAvzbYLxQb9oa5XxpJtKzwOHxKU9CYSXS3cUPKFvAh8mo39w3338FiZ4kuVIyiJqKx29KsUrgCGn/i7KEFhrLa3Is5yO1AkhZ1UzO1iRKwKXh0ZAhS3CPcR/chpvHOItwJqnlEWiMaEL1kGR0qFmjx+IApsPZJPxuhK3BFQ7+J31y0r/bVwM5tDXkcfpkuDtp6UPJiwtk/vHqZ7fJDCgVv+RoH4CSX0Xe/pngNMGlIY0Gr2Xo92StygI41I5AoM23Zl8o9WNCQPjOC1URLdYKek3sC6fanQMvI2t93LBOI9tQP5G+oTLksZ8wGwbFkbcWQS8uIWg==; _abck=FE7ECA3D82160D8A5D784DF793B1601D~0~YAAQM0w5FxHtL1qGAQAAwqZpbwkmBzJdHS51dcP00WAgJXbV40NocvCP3isUjnTWzgnASMeKKcmWN6zO7ELQdqS+f3F6lPteN/cQyapYlrUTgi1eR/eCq556uNg+Jv3B66MPWE4OyZkeVsEIZwMogTwZuftz2+Q0i+kvY7d54+oyeT86mLo+udFFjiLtlBlmxZcs9yxXQUIM/9K9FpMNGsXgE+ONw12VJ4vDbG7SMJpZZbLcfnjhB05q8IInKTFPNdT60oFa9j9pisjyDiAziYKIHG0eh+rxWh7F8G4TUdSv1S2o67CH4If8/Vtp4zbUtNDvZnu3ITNF+TqYVz9u9dYcVVj+gtQu3vC9bLNXyQ8SQuldRj4Ix/HA0q7MmDV9hITBsUWorNaU+hLew3pbbxDvSEL8cKYB~-1~-1~-1; _t_r=1030%2F%2F; persona=default; jd=190223901903; bm_sv=48A9DEDA82530353F5BFBAD7CEC7A6BB~YAAQM0w5Fy3yL1qGAQAAkihqbxLtGqvd6bWCMUEuN0dlm2klXaG+czjgWWnKxxVszpvaAGFhhv/QKku+1hviXO6dEUemvNi1bhtsoafS1gVrO8W1mRE8VitZYpA7Oa4NH3at5PIJvInTjzLRn9mVAV01ClrjdP34zaN2Nr1vyTH0R9tBMsXh6tGqxjky5WUUgSrJQll83zpFG50PI5Rk95Jbf1SbhmI/gL6m6XEd9StajJFq1gqV2ef2UlIKbY1T8g==~1"
+    }
+
+NAUKRI_ALL_JOBS_HEADERS = {
+        "appid": "109",
+        "systemid": "109",
+        "clientid": "d3skt0p",
+        "accept": "application/json",
+        "accept-encoding": "gzip, deflate, br",
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36",
+    }
+
+
+MONSTER_JOB_SEARCH_ENDPOINT = "https://www.foundit.in/middleware/jobsearch"
+MONSTER_JOB_DETAILS_ENDPOINT = "https://www.foundit.in/middleware/jobdetail/"
+MONSTER_API_HEADERS = {
+                "referer": "https://www.foundit.in/srp/results",
+                "accept": "application/json",
+                "accept-encoding": "gzip, deflate, br",
+                "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36",
+            }
+
+MONSTER_PAYLOAD = {
+            "sort": "1",
+            "limit": "100",
+            "start": "0",
+            "locations": "india"
+        }
