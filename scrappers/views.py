e7203ad21c064536105f1abf6d6e0afec39095a6
Sayak Saha <83216382+sayakongit@users.noreply.github.com>
2023-03-22 02:58:09 +0530

    updated celery settings

--- scrappers/views.py
+++ scrappers/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .tasks import test_func
 # Create your views here.
+
+def test(request):
+    test_func.delay()
+    return HttpResponse("Done")
+    
